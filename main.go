e1db2833063a9da04a1b5f4560f2b3029aec933d
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-09-23 21:20:09 +0700

    create : connection to db

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/ThanyawitNiti/go-fiber-crm/database"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
 // create route
-func setupRoutes() {
+func setupRoutes(app *fiber.App) {
 	app.Get(GetLeads)
+	app.Get(GetLead)
 	app.Post(NewLead)
-	app.Delete(deleteLead)
+	app.Delete(DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("mysql", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
 	app := fiber.New()
+	initDatabase()
 	setupRoutes(app)
+	app.Listen(3000)
+
+	defer database.DBConn.Close() //Close connection when finish
+
 }
