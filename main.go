a8155783ec869a825a1064e601c3e4d90d04db6b
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-09-23 21:01:37 +0700

    create : route

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-//create route
+import (
+	"github.com/gofiber/fiber"
+)
+
+// create route
+func setupRoutes() {
+	app.Get(GetLeads)
+	app.Post(NewLead)
+	app.Delete(deleteLead)
+}
 
 func main() {
-
+	app := fiber.New()
+	setupRoutes(app)
 }
